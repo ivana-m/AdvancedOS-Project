fe08c94e649702dbeee11b389b2481867513597a
Jeremy Archer <open-source@fatlotus.com>
2015-11-17 15:27:41 -0600

    Add rudimentary linear program.

--- greedyTest.py
+++ greedyTest.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-workload = [10] * 6
-speed = [[[0, 1, 2, 3]] * 3] * 6
-powerusage = [[[1, 2, 3, 4]] * 3] * 6
+workload = [10] * 3
+speed = [[[0, 1, 2, 3]] * 3] * 3
+powerusage = [[[1, 2, 3, 4]] * 3] * 3
 powercap = 7
 idlestates = [0] * 3
 idleusage = [1] * 3
+
+import linprog; linprog.linear_program(
+    workload, speed, powerusage, idlestates, idleusage, powercap)

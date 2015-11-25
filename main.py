acfd5cc3c1947cc6c08ff2194c9b3891c9052b41
Ivana Marincic <imarincic@uchicago.edu>
2015-11-25 16:02:01 -0600

    integrated drawing

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import greedy
 import greedypolicies
 import fileio
+import rectangles
 import os
+
+import matplotlib.pyplot as plt #use for drawing
+import matplotlib.patches as patches #use for drawing
 
 #class Task(object):
     #def __init__(self, index, configIndex, power, speed):
@@ -55,9 +59,13 @@ totalWkld = 10
 idlestates = [1024] * numMachines
 idleusage = [idlePow] * numMachines
 
-policies = [greedypolicies.fast, greedypolicies.short, greedypolicies.economic, greedypolicies.earlycompletion, greedypolicies.fastNeconomic, greedypolicies.shortNeconomic, greedypolicies.leasttotalusage]
+#policies = [greedypolicies.fast, greedypolicies.short, greedypolicies.economic, greedypolicies.earlycompletion, 
+            
+policies = [greedypolicies.fastNeconomic, greedypolicies.shortNeconomic, greedypolicies.leasttotalusage, greedypolicies.long, greedypolicies.makelongshort, greedypolicies.longNeconomic, greedypolicies.longNeconomicNfast]
 
-policiesList = ["fast", "short", "economic", "earlycompletion", "fastNeconomic", "shortNeconomic", "leasttotalusage"]
+#policiesList = ["fast", "short", "economic", "earlycompletion", 
+                
+policiesList = ["fastNeconomic", "shortNeconomic", "leasttotalusage", "long", "makelongshort", "longNeconomic", "longNeconomicNfast"]
 
 def generate_files():
     for policy in policies:
@@ -76,17 +84,76 @@ def generate_files():
                 #fileio.val2file(outS, pu, idlestates, idleusage, powerCap, orderS, trnS, runS)
                 
 
-generate_files()
+#generate_files()
+
+
+
+colors = ['r','g','b','y', 'c', 'm']
+idleCol = '0.7'
+
+#use this at the very beginning
+def init_figure():
+    plt.ioff()
+    fig = plt.figure(figsize=(17,15),dpi=100) #values obtained by handtweaking, but might be subject to change. size works well for 26 applications at once. For a smaller image, just remove the arguments and use:
+    #fig = plt.figure() #instead
+    return fig
+
+def add_rectangle(ax, x, y, w, h, col):
+    ax.add_patch(
+        patches.Rectangle(
+            (x,y), w, h,
+            edgecolor = 'none',
+            facecolor = col
+            )
+        )
+    
+#if you want to save the output of the figure as an image, use this at the very end
+def save_figure(fig, fileName):
+    fig.savefig(fileName+'.png', dpi='figure', bbox_inches='tight') 
+    plt.close() #close plot - prevents plot from displaying. Use if generating many plots at once
+    
+def add_label(ax, text, x, y, fontsize):
+    ax.text(x,y,text,fontsize=fontsize)
           
-def process_files():
+def draw(f, fname, cols, idlecol):
+    fig = init_figure()
+    ax = fig.add_subplot(111)
+    
+    tasks, machines, configs, pwrcap, mintime, maxtime, rects = fileio.file2rects(f)    
+    
+    def drawTask(i, j, k, x1, x2, y1, y2):
+        col = idlecol
+        if(i < tasks):
+            col = cols[i % len(cols)]
+        add_rectangle(ax, x1, y1, (x2-x1), (y2-y1), col)
+        
+    rectangles.drawrects(rects, drawTask)
+    ax.axhline(y = pwrcap, c = 'r', linewidth=1, zorder=5)
+    plt.plot([maxtime,maxtime],[0,pwrcap], '0.75')
+    save_figure(fig, fname)
+    
+    
+
+def process_files(dir):
     for policy in policies:
         for numTasks in range(1, totalNumTasks + 1):
             for powerCap in range(1000, maxPowerCap, 400):
-                fname = os.path.join(greedyOut, policiesList[policies.index(policy)]+"-"+str(powerCap)+"-"+str(numTasks))
+                fn = policiesList[policies.index(policy)]+"-"+str(powerCap)+"-"+str(numTasks)
+                fname = os.path.join(dir, fn)
                 
                 f = open(fname, 'r')
+                draw(f, fn, cols, idlecol)
                 
-                fileio.file2rects(f)
+
+#f = "output/greedy/fast-1000-7"
+#draw(f, "fast-1000-7", colors, idleCol)
+
+
+
+                
+          
+                
+                
                 
   
     

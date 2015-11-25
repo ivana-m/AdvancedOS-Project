5f3dafd2214a85ef338bad64d745c48c243916bd
lenacore <lenacore@gmail.com>
2015-11-24 21:11:13 -0600

    [Bug+] Adds new policies for greedy and fixes a bug
    Bug: typo in fastNeconomic

--- greedypolicies.py
+++ greedypolicies.py
@@ -12,10 +12,16 @@ def short(wrkld, spd, pwrusg, t1, t2):
     i2,j2,k2 = t2
     return wrkld[i1] < wrkld[i2]
 
+def long(wrkld, spd, pwrusg, t1, t2):
+    i1,j1,k1 = t1
+    i2,j2,k2 = t2
+    return wrkld[i1] > wrkld[i2]
+
 def economic(wrkld, spd, pwrusg, t1, t2):
     i1,j1,k1 = t1
     i2,j2,k2 = t2
     return pwrusg[i1][j1][k1] < pwrusg[i2][j2][k2]
+
 
 #These are the 'somewhat make sense' ones:
 def earlycompletion(wrkld, spd, pwrusg, t1, t2):
@@ -26,6 +32,10 @@ def earlycompletion(wrkld, spd, pwrusg, t1, t2):
     # but we make it like this to avoid zero division
     return wrkld[i1] * spd[i2][j2][k2] < wrkld[i2] * spd[i1][j1][k1]
 
+def makelongshort(wrkld, spd, pwrusg, t1, t2):
+    i1,j1,k1 = t1
+    i2,j2,k2 = t2
+    return wrkld[i1] * spd[i1][j1][k1] > wrkld[i2] * spd[i2][j2][k2]
 
 def fastNeconomic(wrkld, spd, pwrusg, t1, t2):
     i1,j1,k1 = t1
@@ -33,12 +43,21 @@ def fastNeconomic(wrkld, spd, pwrusg, t1, t2):
     # should be
     # return pwrusg[i1][j1][k1] / spd[i1][j1][k1] < pwrusg[i2][j2][k2] / spd[i2][j2][k2]
     # but we make it like this to avoid zero division
-    return pwrusg[i1][j1][k1] / spd[i1][j1][k1] < pwrusg[i2][j2][k2] / spd[i2][j2][k2]
+    return pwrusg[i1][j1][k1] * spd[i2][j2][k2] < pwrusg[i2][j2][k2] * spd[i1][j1][k1]
 
 def shortNeconomic(wrkld, spd, pwrusg, t1, t2):
     i1,j1,k1 = t1
     i2,j2,k2 = t2
     return wrkld[i1] * pwrusg[i1][j1][k1] < wrkld[i2] * pwrusg[i2][j2][k2]
+
+def longNeconomic(wrkld, spd, pwrusg, t1, t2):
+    i1,j1,k1 = t1
+    i2,j2,k2 = t2
+    # should be
+    # return pwrusg[i1][j1][k1] / wrkld[i1] < pwrusg[i2][j2][k2] / wrkld[i2]
+    # but we make it like this to avoid zero division
+    return pwrusg[i1][j1][k1] * wrkld[i2] < pwrusg[i2][j2][k2] * wrkld[i1]
+
 
 #These are the ones that are not blind to any parameter:
 def leasttotalusage(wrkld, spd, pwrusg, t1, t2):
@@ -48,3 +67,11 @@ def leasttotalusage(wrkld, spd, pwrusg, t1, t2):
     # return wrkld[i1] * pwrusg[i1][j1][k1] / spd[i1][j1][k1] < wrkld[i2] * pwrusg[i2][j2][k2] / spd[i2][j2][k2]
     # but we make it like this to avoid zero division
     return wrkld[i1] * pwrusg[i1][j1][k1] * spd[i2][j2][k2] < wrkld[i2] * pwrusg[i2][j2][k2] * spd[i1][j1][k1]
+
+def longNeconomicNfast(wrkld, spd, pwrusg, t1, t2):
+    i1,j1,k1 = t1
+    i2,j2,k2 = t2
+    # should be
+    # return pwrusg[i1][j1][k1] /(spd[i1][j1][k1] * wrkld[i1]) < pwrusg[i2][j2][k2] /(spd[i2][j2][k2] * wrkld[i2])
+    # but we make it like this to avoid zero division
+    return pwrusg[i1][j1][k1] * spd[i2][j2][k2] * wrkld[i2] < pwrusg[i2][j2][k2] * spd[i1][j1][k1] * wrkld[i1]

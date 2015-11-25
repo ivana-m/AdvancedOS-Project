27c1bf254e22a4e7d502e590d6cd732cae597401
lenacore <lenacore@gmail.com>
2015-11-24 21:09:55 -0600

    [Minor, bug] Fixes greedyTest

--- greedyTest.py
+++ greedyTest.py
@@ -7,6 +7,3 @@ powerusage = [[[1, 2, 3, 4]] * 3] * 3
 powercap = 7
 idlestates = [0] * 3
 idleusage = [1] * 3
-
-import linprog; linprog.linear_program(
-    workload, speed, powerusage, idlestates, idleusage, powercap)
